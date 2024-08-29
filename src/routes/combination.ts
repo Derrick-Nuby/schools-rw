04c8869a8f258a241cc714296fbe38d7fa9b3b9a
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-08-29 19:15:33 +0200

    updated with user authentication

--- src/routes/combination.ts
+++ src/routes/combination.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { getAllCombinations, getSingleCombination, createCombination, updateCombination, deleteCombination } from "../controllers/combination.js";
 import { validateCombination, validateUpdateCombination } from "../middleware/combinationValidation.js";
-// import { adminAuthJWT, userAuthJWT } from '../middleware/auth.js';
+import { adminAuthJWT, userAuthJWT } from '../middleware/auth.js';
 
 const router: Router = Router();
 
 router.get("/", getAllCombinations);
 router.get("/:id", getSingleCombination);
-router.post("/", validateCombination, createCombination);
-router.put("/:id", validateUpdateCombination, updateCombination);
-router.delete("/:id", deleteCombination);
+router.post("/", validateCombination, adminAuthJWT, createCombination);
+router.put("/:id", validateUpdateCombination, adminAuthJWT, updateCombination);
+router.delete("/:id", adminAuthJWT, deleteCombination);
 
 export default router;
